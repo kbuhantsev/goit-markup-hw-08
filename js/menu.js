ad1f202d9cccf0ba047fdb8315a849b9a2b5d915
Konstantyn <k.buhantsev@gmail.com>
2022-07-23 16:23:45 +0300

    fix bugs from menthor

--- js/menu.js
+++ js/menu.js
@@ -10,5 +10,6 @@
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
+    document.querySelector("[data-page]").classList.toggle("no-scroll");
   });
 })();
